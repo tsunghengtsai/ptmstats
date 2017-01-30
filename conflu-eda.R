b9c84ded9b93d89745374efefa51004af55a6c02
Tsung-Heng Tsai <tsai.tsungheng@gmail.com>
2017-01-30 12:50:12 -0500

    Filter out peptides of length <6 AA. Some reformatting

--- conflu-eda.R
+++ conflu-eda.R
@@ -1,4 +1,3 @@
-## load required packages -------------------------------------------
 library(readr)
 library(tibble)
 library(tidyr)
@@ -6,14 +5,8 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 
-
-## set up working directory and load datasets -----------------------
-# work_dir <- "/Users/thtsai/Projects/NEU/ptm/dev/"
-# data_dir <- "/Users/thtsai/Projects/NEU/ptm/data/CCCP_confluency/"
-# setwd(work_dir)
-
-
-## vista outputs
+# Prepare data set --------------------------------------------------------
+# Load experimental design with data filenames
 df_design <- read.table("data/CCCP_confluency/CellDensity_tft_forTH.txt", 
                         header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
@@ -24,6 +17,7 @@ sub_cols <- c("Reference", "run_id", "peptide_id", "vista_peak_area_light",
               "peptide_trypticity", "peptide_miscleavages", "peptide_validity", 
               "label1", "ms2_rt", "ms2_charge", "is_xq")
 
+# Prepare data set with columns of interest for later use
 dta_conflu <- vector("list", length(file_name))
 for (i in seq_along(file_name)) {
     file_path <- paste0("data/CCCP_confluency/", file_name[i])
@@ -34,19 +28,16 @@ dta_conflu <- bind_rows(dta_conflu)
 rm(list = c("i", "file_name", "file_path"))
 
 # save.image("output/dta_conflu.RData")
-# save.image("~/Projects/NEU/ptm/dev/dta_conflu.RData")
 
 
-## starting workflow ------------------------------------------------
-## load dataset
-# work_dir <- "/Users/thtsai/Projects/NEU/ptm/dev/"
-# setwd(work_dir)
+# Load data set & housekeeping --------------------------------------------
 load("output/dta_conflu.RData")
 
-dta_conflu <- dta_conflu %>% 
-    filter(vista_confidence_score >= 83)
+# Confidence threshold
+dta_conflu <- dta_conflu %>% filter(vista_confidence_score >= 83)
 
-# df_design %>% arrange(cond_treatment, cond_confluency, id_subject, id_injectionset) %>% 
+# Experimental design 
+# df_design %>% arrange(id_subject, ann_objective_id, id_injectionset) %>%
 #     select(-ann_label)
 
 df_design <- tbl_df(df_design)
@@ -55,7 +46,7 @@ dta_conflu <- dta_conflu %>%
     select(-vista_confidence_score, -peptide_trypticity, -peptide_miscleavages, 
            -peptide_validity)
 
-## OBJ# can be viewed as a batch identity
+# OBJ# can be viewed as a batch identity
 uniq_subj <- unique(df_design$id_subject)
 key_subj <- paste0("S", sprintf("%02d", 1:length(uniq_subj)))
 df_design <- df_design %>% 
@@ -66,8 +57,8 @@ df_design <- df_design %>%
            run_cbt = paste(id_subject, run_bt, sep = "-"))
 
 
-## data annotation and filtering ------------------------------------
-## [TODO]: need a closer look at `Reference` & `label1`
+# Annotation & data filtering ---------------------------------------------
+# [TODO]: need a closer look at `Reference` & `label1`
 dta_conflu <- dta_conflu %>% 
     mutate(is_mod = grepl("\\*", peptide_sequence), 
            feature = paste(peptide_sequence, ms2_charge, sep = "_"), 
@@ -77,96 +68,93 @@ dta_conflu <- dta_conflu %>%
            techrep = plyr::mapvalues(run_id, from = df_design$run_id, to = df_design$techrep), 
            run_cbt = plyr::mapvalues(run_id, from = df_design$run_id, to = df_design$run_cbt))
 
-## features with no charge state or retention time inforamtion
+# Extract original peptide sequence & other information
+df_pep <- dta_conflu %>% 
+    distinct(peptide_sequence) %>% 
+    mutate(unmod_peptide = str_replace_all(peptide_sequence, "\\*", ""), 
+           trim_peptide = str_match(unmod_peptide, regex("\\.[ACDEFGHIKLMNPQRSTVWY]+")) %>% 
+               str_replace("\\.", ""), 
+           nb_aa = str_length(trim_peptide))
+
+# Ensure each peptide was extracted correctly
+# df_pep %>% filter(is.na(nb_aa) | nb_aa == 0)
+
+# Features with no charge state or retention time inforamtion
 # dta_conflu %>% filter(is.na(ms2_charge) | is.na(ms2_rt)) %>%
 #     select(feature, peptide_sequence, ms2_charge, ms2_rt)
 
-## short matches
-# dta_conflu %>% filter(grepl("-\\..$", peptide_sequence)) %>%
-#     select(feature, peptide_sequence)
+# Short matches (minimum acceptable length of 6 AAs)
+# dta_conflu %>% semi_join(filter(df_pep, nb_aa < 6)) %>% 
+#     distinct(peptide_sequence, feature)
 
-## not belonging to expected uniprot ID
-regex_uniprot <- ".*([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}).*"
+# Unexpected uniprot ID (non-human proteins in this case)
+# regex_uniprot <- ".*([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}).*"
 # dta_conflu %>% filter(!grepl(pattern = regex_uniprot, Reference))
 
-## preliminary filtering
+# Initial filtering
+regex_uniprot <- ".*([OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}).*"
 dta_conflu <- dta_conflu %>%
-    filter(!is.na(ms2_charge), !is.na(ms2_rt), !grepl("-\\..$", peptide_sequence), grepl(regex_uniprot, Reference))
+    semi_join(filter(df_pep, !(nb_aa < 6))) %>% 
+    filter(!is.na(ms2_charge), !is.na(ms2_rt), grepl(regex_uniprot, Reference))
 
-
-## extract uniprot accession number
+# Extract uniprot accession number
 uniq_ref <- dta_conflu %>% distinct(Reference) %>% .$Reference
 uniq_unip <- unlist(lapply(uniq_ref, function(x) str_extract(unlist(str_split(x, '\\|'))[2], regex_uniprot)))
 dta_conflu <- dta_conflu %>% 
     mutate(uniprot_ac = plyr::mapvalues(Reference, from = uniq_ref, to = uniq_unip))
 
 
-## data exploration -------------------------------------------------
-## note the proportion of modified peptide in different biological replicates
+# Data exploration --------------------------------------------------------
+# Proportion of modified/unmodified peptides in each run
 table(dta_conflu$run_cbt, dta_conflu$is_mod)
 
-## check for duplicate features 
+# Check for duplicate features (4971 in total)
 dta_conflu %>% 
     count(run_id, feature) %>% ungroup() %>% 
     filter(n > 1)
 
-## 2158 features from 16 runs have duplicates (4984 features in total)
+# 2151 unique features with duplicates
 dta_conflu %>% 
     count(run_id, feature) %>% ungroup() %>% 
     filter(n > 1) %>% 
     distinct(feature)
 
-## 814 with additional criterion for max RT diff > 1.5 min
+# 813 with additional criterion for max RT diff > 1.5 min
 dta_conflu %>% 
     group_by(run_id, feature) %>% 
     summarise(n = n(), d_rt = max(ms2_rt, na.rm = TRUE) - min(ms2_rt, na.rm = TRUE)) %>% 
     ungroup() %>% 
     filter(n > 1, d_rt > 1.5)
 
-## check for mismatched features (different RT in different runs for the same peptide)
-## to be carried out after duplicates within run are handled
-# dta_conflu %>% 
-#     group_by(feature) %>% 
-#     summarise(d_rt = max(ms2_rt, na.rm = TRUE) - min(ms2_rt, na.rm = TRUE)) %>% 
-#     ungroup() %>% 
-#     filter(d_rt > 5)
-
-
-## protein & protein group
-length(unique(dta_conflu$Reference))
-
-
-## protein (Reference)
-## [TODO]: check if uniprot # is a better proxy
-dta_conflu %>% select(Reference, run_id, peptide_sequence) %>% distinct() %>% 
-    group_by(run_id, peptide_sequence) %>% mutate(nb_prot = n()) %>% ungroup() %>% 
-    filter(nb_prot > 1) %>% arrange(peptide_sequence)
-
+# Protein (Reference)
 dta_conflu %>% select(Reference, peptide_sequence) %>% distinct() %>% 
     group_by(peptide_sequence) %>% mutate(nb_prot = n()) %>% ungroup() %>% 
     filter(nb_prot > 1) %>% arrange(peptide_sequence)
 
+# Uniprot ID ([TODO]: address the aliasing pairs)
+dta_conflu %>% select(uniprot_ac, peptide_sequence) %>% distinct() %>% 
+    group_by(peptide_sequence) %>% mutate(nb_prot = n()) %>% ungroup() %>% 
+    filter(nb_prot > 1) %>% arrange(peptide_sequence)
 
-## filtering before checking intensity profile ----------------------
-## use the max for duplicate features (per run)
-## [TODO]: conisder is_xq, ms2_rt
+
+
+# Data manipulation & transformation --------------------------------------
+# Use the max for duplicate features (per run)
+# [TODO]: conisder is_xq, ms2_rt
 dta_conflu <- dta_conflu %>% 
     group_by(Reference, uniprot_ac, peptide_sequence, is_mod, feature, ms2_charge, cond, biorep, run_cbt, run_id) %>% 
     summarise(log2inty = max(log2inty)) %>% 
     ungroup()
 
-
-## data manipulation and transformation -----------------------------
-## remove peptides with ambiguous matches (to >1 uniprot_ac) for now
-nb_pep2uniprot <- dta_conflu %>% 
+# Remove peptides with ambiguous matches (to >1 uniprot_ac) for now
+pep2uniprot <- dta_conflu %>% 
     distinct(peptide_sequence, uniprot_ac) %>% 
     count(peptide_sequence)
 
 dta_conflu <- dta_conflu %>% 
-    semi_join(filter(nb_pep2uniprot, n == 1))
+    semi_join(filter(pep2uniprot, n == 1))
 
-
-## normalization (equalizing median)
+# normalization (equalizing median)
 med_run <- dta_conflu %>% 
     group_by(run_id) %>% 
     summarise(log2inty_med = median(log2inty, na.rm = TRUE)) %>% 
