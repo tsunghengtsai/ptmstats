b13afbf73deab93e8182efbf629ad086b1565a2f
Tsung-Heng Tsai <tsai.tsungheng@gmail.com>
2017-06-25 22:12:41 -0400

    Add functions for differential analysis

--- utilities.R
+++ utilities.R
@@ -199,3 +199,157 @@ tidy_bch <- function(bch_fit, df_bch) {
     return(params)
 }
 
+
+# Testing for differential modification
+test_mod <- function(df_mod, grp_ctrl, grp_case) {
+    full_mod <- df_mod %>% 
+        filter(group %in% c(grp_ctrl, grp_case)) %>% 
+        filter(!is.na(df_res)) %>% 
+        mutate(key_grp = ifelse(group == grp_ctrl, "G0", "G1")) %>% 
+        complete(protsite, key_grp)
+    # Model-based inference of log-difference
+    diff_mod <- full_mod %>% 
+        group_by(protsite) %>% 
+        filter(!any(is.na(df_res))) %>% 
+        group_by(protsite, df_res) %>% 
+        summarise(log2fc = diff(estimate), se2 = sum(std.error ^ 2)) %>% 
+        ungroup()
+    # Missing in one group
+    part_mod <- full_mod %>% anti_join(diff_mod)
+    untest_mod <- part_mod %>% 
+        filter(!is.na(df_res)) %>% 
+        distinct(protsite, key_grp) %>% 
+        mutate(
+            logFC = ifelse(key_grp == "G1", Inf, -Inf), 
+            SE = NA, DF = NA, t_stat = NA, p_val = NA, 
+            ctrx = paste(grp_case, grp_ctrl, sep = " vs ")
+        ) %>% select(-key_grp)
+    res <- diff_mod %>% 
+        mutate(logFC = log2fc, SE = sqrt(se2), DF = df_res, 
+               t_stat = logFC / SE, p_val = 2 * pt(abs(t_stat), df = DF, lower.tail = FALSE)) %>% 
+        select(protsite, logFC, SE, DF, t_stat, p_val) %>% 
+        mutate(ctrx = paste(grp_case, grp_ctrl, sep = " vs ")) %>% 
+        bind_rows(untest_mod)
+    
+    return(res)
+}
+
+
+# Testing for differential modification with adjustment
+test_adjmod <- function(df_mod, grp_ctrl, grp_case) {
+    full_mod <- df_mod %>% 
+        filter(group %in% c(grp_ctrl, grp_case)) %>% 
+        filter(!is.na(df_res)) %>% 
+        mutate(key_grp = ifelse(group == grp_ctrl, "G0", "G1")) %>% 
+        complete(protsite, key_grp)
+    # Model-based inference of log-difference
+    diff_mod <- full_mod %>% 
+        group_by(protsite) %>% 
+        filter(all(!is.na(df_res)), all(!is.na(df_unmod))) %>% 
+        group_by(protsite, df_res, df_unmod) %>% 
+        summarise(
+            log2fc = diff(estimate), se2 = sum(std.error ^ 2), 
+            log2fc_unmod = diff(est_unmod), se2_unmod = sum(se_unmod ^ 2)
+        ) %>% ungroup()
+    # Missing in one group
+    part_mod <- full_mod %>% anti_join(diff_mod)
+    untest_mod <- part_mod %>% 
+        filter(!is.na(df_res), !is.na(df_unmod)) %>% 
+        distinct(protsite, key_grp) %>% 
+        mutate(
+            logFC = ifelse(key_grp == "G1", Inf, -Inf), 
+            SE = NA, DF = NA, t_stat = NA, p_val = NA, 
+            ctrx = paste(grp_case, grp_ctrl, sep = " vs ")
+        ) %>% select(-key_grp)
+    res <- diff_mod %>% 
+        mutate(logFC = log2fc - log2fc_unmod, SE = sqrt(se2 + se2_unmod), 
+               DF = (se2 + se2_unmod) ^ 2 / (se2 ^ 2 / df_res + se2_unmod ^ 2 / df_unmod), 
+               t_stat = logFC / SE, p_val = 2 * pt(abs(t_stat), df = DF, lower.tail = FALSE)) %>% 
+        select(protsite, logFC, SE, DF, t_stat, p_val) %>% 
+        mutate(ctrx = paste(grp_case, grp_ctrl, sep = " vs ")) %>% 
+        bind_rows(untest_mod)
+    
+    return(res)
+}
+
+
+# Batch-aggregated testing for differential modification
+test_mod_bch <- function(df_mod, grp_ctrl, grp_case) {
+    full_mod <- df_mod %>% 
+        filter(group %in% c(grp_ctrl, grp_case)) %>% 
+        filter(!is.na(df_res)) %>% 
+        mutate(key_grp = ifelse(group == grp_ctrl, "G0", "G1")) %>% 
+        complete(protsite, batch, key_grp)
+    # Model-based inference of log-difference
+    diff_mod <- full_mod %>% 
+        group_by(protsite) %>% 
+        filter(all(!is.na(df_res))) %>% 
+        group_by(protsite, batch, df_res) %>% 
+        summarise(log2fc = diff(estimate), se2 = sum(std.error ^ 2)) %>% 
+        ungroup()
+    # Missing in one group
+    part_mod <- full_mod %>% anti_join(diff_mod)
+    untest_mod <- part_mod %>% 
+        group_by(protsite, key_grp) %>% 
+        filter(all(!is.na(df_res))) %>% 
+        ungroup() %>% 
+        distinct(protsite, key_grp) %>% 
+        mutate(
+            logFC = ifelse(key_grp == "G1", Inf, -Inf), 
+            SE = NA, DF = NA, t_stat = NA, p_val = NA, 
+            ctrx = paste(grp_case, grp_ctrl, sep = " vs ")
+        ) %>% select(-key_grp)
+    res <- diff_mod %>% 
+        group_by(protsite) %>% 
+        summarise(logFC = mean(log2fc), SE = sqrt(sum(se2)) / n(), 
+                  DF = sum(se2) ^ 2 / sum(se2 ^ 2 / df_res), 
+                  t_stat = logFC / SE, p_val = 2 * pt(abs(t_stat), df = DF, lower.tail = FALSE)) %>% 
+        mutate(ctrx = paste(grp_case, grp_ctrl, sep = " vs ")) %>% 
+        bind_rows(untest_mod)
+    
+    return(res)
+}
+
+
+# Batch-aggregated testing for differential modification with adjustment
+test_adjmod_bch <- function(df_mod, grp_ctrl, grp_case) {
+    full_mod <- df_mod %>% 
+        filter(group %in% c(grp_ctrl, grp_case)) %>% 
+        filter(!is.na(df_res)) %>% 
+        mutate(key_grp = ifelse(group == grp_ctrl, "G0", "G1")) %>% 
+        complete(protsite, batch, key_grp)
+    # Model-based inference of log-difference
+    diff_mod <- full_mod %>% 
+        group_by(protsite) %>% 
+        filter(all(!is.na(df_res)), all(!is.na(df_unmod))) %>% 
+        group_by(protsite, batch, df_res, df_unmod) %>% 
+        summarise(
+            log2fc = diff(estimate), se2 = sum(std.error ^ 2),
+            log2fc_unmod = diff(est_unmod), se2_unmod = sum(se_unmod ^ 2)
+        ) %>% ungroup()
+    # Missing in one group
+    part_mod <- full_mod %>% anti_join(diff_mod)
+    untest_mod <- part_mod %>% 
+        group_by(protsite, key_grp) %>% 
+        filter(all(!is.na(df_res)), all(!is.na(df_unmod))) %>% 
+        ungroup() %>% 
+        distinct(protsite, key_grp) %>% 
+        mutate(
+            logFC = ifelse(key_grp == "G1", Inf, -Inf), 
+            SE = NA, DF = NA, t_stat = NA, p_val = NA, 
+            ctrx = paste(grp_case, grp_ctrl, sep = " vs ")
+        ) %>% select(-key_grp)
+    res <- diff_mod %>% 
+        group_by(protsite) %>% 
+        summarise(
+            logFC = mean(log2fc) - mean(log2fc_unmod), 
+            SE = sqrt(sum(se2) + sum(se2_unmod)) / n(), 
+            DF = (sum(se2) + sum(se2_unmod)) ^ 2 / sum(se2 ^ 2 / df_res + se2_unmod ^ 2 / df_unmod), 
+            t_stat = logFC / SE, p_val = 2 * pt(abs(t_stat), df = DF, lower.tail = FALSE)
+        ) %>% 
+        mutate(ctrx = paste(grp_case, grp_ctrl, sep = " vs ")) %>% 
+        bind_rows(untest_mod)
+    
+    return(res)
+}
+
